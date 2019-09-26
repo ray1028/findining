bbe5b19e43f881032503cd358d27fd74b852e011
jimmyzmli <jimmyzmli@outlook.com>
2019-09-25 20:17:18 -0400

    Add expo push notification dependencies

---
+++ src/gcm.js
@@ -0,0 +1,76 @@
+import {Notifications} from "expo";
+import * as Permissions from "expo-permissions";
+import React from "react";
+import {Text, View} from "react-native";
+
+registerForPushNotificationsAsync = async() => {
+  console.log("1");
+  const {status: existingStatus} = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+  let finalStatus = existingStatus;
+
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== "granted") {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== "granted") {
+    return;
+  }
+  console.log("2");
+
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+
+  // POST the token to your backend server from where you can retrieve it to send push notifications.
+  console.log("PUSH TOKEN", token);
+  // return fetch(PUSH_ENDPOINT, {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json"
+  //   },
+  //   body: JSON.stringify({
+  //     token: {
+  //       value: token
+  //     },
+  //     user: {
+  //       username: "Brent"
+  //     }
+  //   })
+  // });
+}
+
+export default class PushMessage extends React.Component {
+  state = {
+    notification: {},
+  };
+
+  componentDidMount() {
+    registerForPushNotificationsAsync();
+
+    // Handle notifications that are received or selected while the app
+    // is open. If the app was closed and then opened by tapping the
+    // notification (rather than just tapping the app icon to open it),
+    // this function will fire on the next tick after the app starts
+    // with the notification data.
+    this._notificationSubscription = Notifications.addListener(this._handleNotification);
+  }
+
+  _handleNotification = (notification) => {
+    this.setState({notification: notification});
+  };
+
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Origin: {this.state.notification.origin}</Text>
+        <Text>Data: {JSON.stringify(this.state.notification.data)}</Text>
+      </View>
+    );
+  }
+}
